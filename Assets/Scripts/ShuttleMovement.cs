2205bd1b5472e19527fcd7b218562d7999270c42
JosephGlz <joseph220199@gmail.com>
2023-11-22 23:34:23 -0600

    moon added, saturn rigns, boost to spaceshuttle

--- Assets/Scripts/ShuttleMovement.cs
+++ Assets/Scripts/ShuttleMovement.cs
@@ -4,28 +4,29 @@ using UnityEngine;
 
 public class ShuttleMovement : MonoBehaviour
 {
-    public float forwardSpeed = 1.0f; // Velocidad hacia adelante
-    public float verticalRotationSpeed = 50.0f; // Velocidad de rotación vertical
-    public float rollSpeed = 100.0f; // Velocidad de giro
+    [SerializeField] float forwardSpeed = 1.0f;
+    [SerializeField] float verticalRotationSpeed = 50.0f;
+    [SerializeField] float rollSpeed = 100.0f;
 
     void Start()
     {
-        // Ocultar el cursor al iniciar el juego
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
     void Update()
     {
-        // Movimiento hacia adelante constante
-        transform.Translate(Vector3.forward * forwardSpeed * Time.deltaTime);
-
-        // Rotación hacia arriba (S) o hacia abajo (W)
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+           transform.Translate(Vector3.forward * forwardSpeed * 20 * Time.deltaTime);
+        } else
+        {
+            transform.Translate(Vector3.forward * forwardSpeed * Time.deltaTime);
+        }
+        //transform.Translate(Vector3.forward * forwardSpeed * Time.deltaTime);
         float verticalInput = Input.GetAxis("Vertical");
         float verticalRotation = verticalInput * verticalRotationSpeed * Time.deltaTime;
         transform.Rotate(verticalRotation, 0, 0);
-
-        // Rotación en el eje Z con A y D
         float rollInput = Input.GetAxis("Horizontal");
         float rollAmount = rollInput * rollSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -rollAmount);
