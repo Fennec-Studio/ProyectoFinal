240c5959fc211afb86e2eea201b4c00df4ad7c18
JosephGlz <joseph220199@gmail.com>
2023-11-27 19:19:11 -0600

    Planet bloom

--- Assets/Scripts/PlanetTraslation.cs
+++ Assets/Scripts/PlanetTraslation.cs
@@ -6,9 +6,18 @@ public class PlanetTraslation : MonoBehaviour
 {
     [SerializeField] Transform planetOrbit;
     [SerializeField] float speedTraslation = 20f;
+    private TrailRenderer planetTrail;
+    private void Start()
+    {
+        planetTrail = GetComponent<TrailRenderer>();
+    }
 
     void Update()
     {
         transform.RotateAround(planetOrbit.position, Vector3.up, speedTraslation * Time.deltaTime);
+        if(planetTrail != null )
+        {
+            planetTrail.transform.position = transform.position;
+        }
     }
 }
